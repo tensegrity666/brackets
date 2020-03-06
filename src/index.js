3560a8f3d96116ff345b10984fc8c85f6683e8be
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-06 23:11:13 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  let bracketsPair = [];
+  const length = str.length;
 }
