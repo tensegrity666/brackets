ad09e00cb38e071e21cb899452623c515c2890c3
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-06 23:58:45 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
 const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 
 module.exports = function check(str, bracketsConfig) {
-  str = str.split('');
-  let bracketsPair = [];
-  const length = str.length;
+  let bracketsPair = bracketsConfig.map(item => item.join(''));
+  let length = str.length;
+
+  for (i = 0; i < length; i++) {
+    for (let j = 0; j < bracketsPair.length; j++) {
+      if (str.includes(bracketsPair[j])) {
+        str = str.replace(bracketsPair[j], '');
+        console.log(str);
+        --length;
+      }
+    }
+  }
+
+  return (str === '') ? true : false;
+
 }
